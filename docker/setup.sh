8e50822564919f1b8f5166520bf0d43627ac060b
OsawaKousei <jwithelis@gmail.com>
2024-04-14 13:02:08 +0900

    initialize

---
+++ docker/setup.sh
@@ -0,0 +1,17 @@
+# update
+sudo apt update
+sudo apt upgrade -y
+
+# environment setup
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+
+# create workspace
+cd ~
+git clone https://github.com/OsawaKousei/ros2_serial_visualizer.git
+echo "source ~/ros2_serial_visualizer/install/setup.bash" >> ~/.bashrc
+cd ~/ros2_serial_visualizer
+colcon build
+cd ~
+
+# refresh bashrc
+source ~/.bashrc
